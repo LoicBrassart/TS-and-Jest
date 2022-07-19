5077a9a823c0a851df16f71e13dbc7feb6664746
LoicBrassart <loic.brassart@gmail.com>
2022-07-19 16:29:14 +0200

    Add first round of tests

---
+++ Character.spec.ts
@@ -0,0 +1,8 @@
+import { Character } from "src/classes";
+
+describe("The Character class", () => {
+  it("can instantiate", () => {
+    const drizzt = new Character(18, 18, 18);
+    expect(drizzt).toBeInstanceOf(Character);
+  });
+});
